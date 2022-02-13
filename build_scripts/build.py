9e8809a4f45e5c24ed4e73246789fe505873be9f
datagutten <datagutten@datagutten.net>
2022-02-13 20:08:32 +0100

    Download zip in workflow

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 import os.path
 import re
-import subprocess
-import sys
 
-url = 'https://updates.duplicati.com/beta/duplicati-2.0.6.3_beta_2021-06-17.zip'
+url = os.getenv('ZIP_URL')
+if not url:
+    raise ValueError('Environment variable ZIP_URL not set')
+
 matches = re.search(r'duplicati-([\d.]+)_(\w+)_', url)
 version = matches.group(1)
+script_folder = os.path.dirname(os.path.realpath(__file__))
 
-folder = '/tmp/_duplicati_spk_build'
-os.mkdir(folder)
-local_zip = os.path.join(folder, 'duplicati_%s.zip' % version)
-
-if not os.path.exists(local_zip):
-    status = subprocess.call(['wget', '--no-check-certificate', '-O', local_zip, url])
-    if status != 0:
-        sys.exit(status)
-
-status = subprocess.call(['unzip', '-q', '-d', folder, local_zip])
-if status != 0:
-    sys.exit(status)
+with open(os.path.join(script_folder, 'INFO_template')) as fp_template:
+    template = fp_template.read()
+    template += '\n' + 'version="%s"' % version
+    with open('INFO', 'w') as fp_info:
+        fp_info.write(template)
+    with open('VERSION', 'w') as fp_version:
+        fp_version.write(version)
